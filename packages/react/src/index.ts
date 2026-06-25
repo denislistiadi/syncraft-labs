71f4a0d6460935eed64711df56d45958e557f76d
Denis Listiadi <denislistiadi24@gmail.com>
2026-06-25 10:00:00 +0700

    feat(react): export useSync hook from index

---
+++ packages/react/src/index.ts
@@ -0,0 +1,29 @@
+/**
+ * @module @syncraft/react
+ *
+ * Public API for the Syncraft React integration.
+ *
+ * @example
+ * ```tsx
+ * import { useSync } from "@syncraft/react";
+ *
+ * function TodoList() {
+ *   const { data, update, isHydrating } = useSync<TodoState>("todos", {
+ *     initialState: { todos: [] },
+ *   });
+ *
+ *   if (isHydrating) return <p>Loading…</p>;
+ *
+ *   return (
+ *     <ul>
+ *       {data?.todos.map((t) => (
+ *         <li key={t.id}>{t.text}</li>
+ *       ))}
+ *     </ul>
+ *   );
+ * }
+ * ```
+ */
+
+export { useSync, destroyStore, _resetRegistry } from "./use-sync.js";
+export type { UseSyncOptions, UseSyncReturn } from "./types.js";
